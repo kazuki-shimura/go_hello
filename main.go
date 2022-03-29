875490176c6edc67befec04b4c9506277250335c
kazuki-shimura <sim202025@icloud.com>
2022-03-29 23:26:05 +0900

    ブラウザ表示

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("Hello world\n")
+	startWeb()
+}
+
+func startWeb() {
+	http.HandleFunc("/hello", handler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello golang!!!")
 }
