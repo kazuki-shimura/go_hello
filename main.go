a9071c0cfa21aa6fbc02558d094e8529db0ce507
kazuki-shimura <sim202025@icloud.com>
2022-03-31 23:58:26 +0900

    go mod難しい

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"study"
 )
 
 func main() {
 	fmt.Printf("Hello world\n")
+	Part1()
 	startWeb()
 }
 
 func startWeb() {
 	http.HandleFunc("/hello", handler)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":3000", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
---
+++ study/part1.go
@@ -0,0 +1,18 @@
+package study
+
+import (
+	"fmt"
+)
+
+/*
+	型の学習
+*/
+func Part1() {
+	// 整数
+	var i int = 100
+	fmt.Println(i + 50)
+
+	//var i2 int64 = 100
+	// 型が違うためコンパイルエラー
+	// fmt.Println(i + i2)
+}
